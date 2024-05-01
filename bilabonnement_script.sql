e1a0bbf57d9196c0b518ab64fdc4ff426e8f8312
olni0002 <145747740+olni0002@users.noreply.github.com>
2024-05-01 11:13:37 +0200

    Update bilabonnement_script.sql

--- bilabonnement_script.sql
+++ bilabonnement_script.sql
@@ -9,9 +9,9 @@ CREATE TABLE car (
     model VARCHAR(255),
     monthly_price FLOAT,
     brand VARCHAR(255),
-    stelnummer FLOAT,
-    co2emissions VARCHAR(255),
-    Equipmentlevel VarChar(255),
+    stel_number FLOAT,
+    co2_emissions VARCHAR(255),
+    equipment_level VarChar(255),
     state ENUM('In storage', 'Being used', 'Under inspection'),
     PRIMARY KEY (car_id)
 );
