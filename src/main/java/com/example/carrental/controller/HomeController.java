2e0ea46864e5e05abe75e3f442f696706b909685
olni0002 <olni0002@stud.kea.dk>
2024-05-11 00:34:50 +0200

    Change packages to lowercase

--- src/main/java/com/example/carrental/controller/HomeController.java
+++ src/main/java/com/example/carrental/controller/HomeController.java
@@ -1,13 +1,16 @@
-package com.example.carrental.controller;
+package com.example.carrental.Controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String index() {
+    public String index(Model model){
+
         return "home/index";
     }
 }
