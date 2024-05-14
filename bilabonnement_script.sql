ec93ad564de7633639ded230bdfe0c65a49156d0
olni0002 <145747740+olni0002@users.noreply.github.com>
2024-05-14 12:09:44 +0200

    Update bilabonnement_script.sql

--- bilabonnement_script.sql
+++ bilabonnement_script.sql
@@ -20,6 +20,7 @@ CREATE TABLE car (
 CREATE TABLE equipment (
     equipment_id INT AUTO_INCREMENT,
     name VARCHAR(255),
+    car_id INT,
     PRIMARY KEY (equipment_id),
     FOREIGN KEY (car_id) REFERENCES car(car_id)
 );
