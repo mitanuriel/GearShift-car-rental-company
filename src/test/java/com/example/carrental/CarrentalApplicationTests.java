83d4353ad3da491410d3de2ddf6e7de7b4b4e686
hungvy <hung0001@stud.kea.dk>
2024-05-28 02:25:59 +0200

    unittest

--- src/test/java/com/example/carrental/CarrentalApplicationTests.java
+++ src/test/java/com/example/carrental/CarrentalApplicationTests.java
@@ -1,13 +1,45 @@
 package com.example.carrental;
 
+import com.example.carrental.model.Car;
+import com.example.carrental.repository.CarRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class CarrentalApplicationTests {
 
+    @Autowired
+    CarRepository carRepository;
+
+
+
     @Test
-    void contextLoads() {
+    void testforseallebiler(){
+        List<Car> cars = carRepository.getAllCars();
+        System.out.println(cars.size());
+        Assertions.assertNotNull(cars);
     }
+
+
+
+    @Test
+    void testdelete(){
+        List<Car> carsBefore = carRepository.getAllCars();
+        int beforeDeleted = carsBefore.size();
+        // Vi skal vælge en bil id at slette, vi vælger en arbitrær værdi fra tabellen.
+        // Vi tager i det her eksempel car id 30
+        carRepository.delete(3);
+        List<Car> carsAfter = carRepository.getAllCars();
+        int afterDeleted = carsAfter.size();
+        Assertions.assertEquals(afterDeleted, beforeDeleted-1);
+        //Succesfully
+    }
+
+
+
 
 }
